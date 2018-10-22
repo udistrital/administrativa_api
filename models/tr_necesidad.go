bb0c3ddfce44f4225f9ff8387a5b5df8b1f4212e
Sebastian Yesid Tabares Amaya <sytabaresa@gmail.com>
2018-10-22 10:22:00 -0500

    version con orm crudo

--- models/tr_necesidad.go
+++ models/tr_necesidad.go
@@ -1,11 +1,9 @@
 package models
 
 import (
-	"fmt"
 	"strconv"
 	"time"
 
-	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 )
 
@@ -25,11 +23,9 @@ type TrEspecificacion struct {
 	RequisitoMinimo       []*RequisitoMinimo
 }
 
-//funcion para la transaccion de solicitudes
-func AddTrNecesidad(m *TrNecesidad) (alertas []Alert, err error) {
+func AddTrNecesidad(m *TrNecesidad) (id int64, err error) {
 	o := orm.NewOrm()
 	o.Begin()
-	var id int64
 
 	//default values
 	if m.Necesidad.ModalidadSeleccion == nil {
@@ -46,101 +42,88 @@ func AddTrNecesidad(m *TrNecesidad) (alertas []Alert, err error) {
 	var a []int
 	_, err = o.Raw("SELECT COALESCE(MAX(numero_elaboracion), 0)+1 FROM administrativa.necesidad WHERE vigencia=" + strconv.Itoa((m.Necesidad.FechaSolicitud).Year()) + ";").QueryRows(&a)
 	m.Necesidad.NumeroElaboracion = a[0]
-	if id, err = o.Insert(m.Necesidad); err == nil {
-		//m.Necesidad.Id = int(id)
-		fmt.Println("Fuentes", m.Ffapropiacion)
-		for _, v := range m.Ffapropiacion {
-			v.Necesidad = &Necesidad{Id: int(id)}
-			//---
-			if _, err = o.Insert(v); err != nil {
-				o.Rollback()
-				beego.Error(err)
-				alertas = append(alertas, Alert{Type: AlertError, Body: "Error: ¡Ocurrió un error al insertar las fuentes de financiamiento!"})
-				return
-			}
-		}
-		//--
-		fmt.Println("Marco", m.MarcoLegalNecesidad)
-		for _, vm := range m.MarcoLegalNecesidad {
-			vm.Necesidad = &Necesidad{Id: int(id)}
-			//---
-			if _, err = o.Insert(vm); err != nil {
-				o.Rollback()
-				beego.Error(err)
-				alertas = append(alertas, Alert{Type: AlertError, Body: "Error: ¡Ocurrió un error al insertar los marcos legales!"})
-				return
-			}
-		}
 
-		m.DependenciaNecesidad.Necesidad = &Necesidad{Id: int(id)}
-		if _, err = o.Insert(m.DependenciaNecesidad); err != nil {
+	if id, err = o.Insert(m.Necesidad); err != nil {
+		o.Rollback()
+		return
+	}
+
+	for _, v := range m.Ffapropiacion {
+		v.Necesidad = &Necesidad{Id: int(id)}
+		//---
+		if id, err = o.Insert(v); err != nil {
 			o.Rollback()
-			beego.Error(err)
-			alertas = append(alertas, Alert{Type: AlertError, Body: "Error: ¡Ocurrió un error al insertar los datos de las dependencias y responsables!"})
 			return
 		}
-		if m.Necesidad.TipoContratoNecesidad.Id == 1 {
+	}
 
-			for _, ve := range m.Especificacion {
-				ve.EspecificacionTecnica.Necesidad = &Necesidad{Id: int(id)}
-				//---
-				if _, err = o.Insert(ve.EspecificacionTecnica); err != nil {
-					o.Rollback()
-					beego.Error(err)
-					alertas = append(alertas, Alert{Type: AlertError, Body: "Error: ¡Ocurrió un error al insertar las especificaciones técnicas!"})
-					return
-				} else {
-					for _, vr := range ve.RequisitoMinimo {
-						vr.EspecificacionTecnica = ve.EspecificacionTecnica
-						//---
-						if _, err = o.Insert(vr); err != nil {
-							o.Rollback()
-							beego.Error(err)
-							alertas = append(alertas, Alert{Type: AlertError, Body: "Error: ¡Ocurrió un error al insertar los requisitos mínimos!"})
-							return
-						}
+	for idx, vm := range m.MarcoLegalNecesidad {
+		vm.Necesidad = &Necesidad{Id: int(idx)}
+		//---
+		if id, err = o.Insert(vm); err != nil {
+			o.Rollback()
+			return
+		}
+	}
+
+	m.DependenciaNecesidad.Necesidad = &Necesidad{Id: int(id)}
+	if id, err = o.Insert(m.DependenciaNecesidad); err != nil {
+		o.Rollback()
+		return
+	}
+	if m.Necesidad.TipoContratoNecesidad.Id == 1 {
+
+		for _, ve := range m.Especificacion {
+			ve.EspecificacionTecnica.Necesidad = &Necesidad{Id: int(id)}
+			//---
+			if id, err = o.Insert(ve.EspecificacionTecnica); err != nil {
+				o.Rollback()
+				return
+			} else {
+				for _, vr := range ve.RequisitoMinimo {
+					vr.EspecificacionTecnica = ve.EspecificacionTecnica
+					//---
+					if id, err = o.Insert(vr); err != nil {
+						o.Rollback()
+						return
 					}
 				}
 			}
 		}
-		if m.Necesidad.TipoContratoNecesidad.Id == 2 {
-			for _, va := range m.ActividadEconomicaNecesidad {
-				va.Necesidad = &Necesidad{Id: int(id)}
-				//---
-				if _, err = o.Insert(va); err != nil {
-					o.Rollback()
-					beego.Error(err)
-					alertas = append(alertas, Alert{Type: AlertError, Body: "Error: ¡Ocurrió un error al insertar las actividades económicas!"})
-					return
-				}
-			}
-			for _, vp := range m.ActividadEspecifica {
-				vp.Necesidad = &Necesidad{Id: int(id)}
-				//---
-				if _, err = o.Insert(vp); err != nil {
-					o.Rollback()
-					beego.Error(err)
-					alertas = append(alertas, Alert{Type: AlertError, Body: "Error: ¡Ocurrió un error al insertar las actividades específicas!"})
-					return
-				}
-			}
-			m.DetalleServicioNecesidad.Necesidad = &Necesidad{Id: int(id)}
-			if _, err = o.Insert(m.DetalleServicioNecesidad); err != nil {
+	}
+	if m.Necesidad.TipoContratoNecesidad.Id == 2 {
+		for _, va := range m.ActividadEconomicaNecesidad {
+			va.Necesidad = &Necesidad{Id: int(id)}
+			//---
+			if id, err = o.Insert(va); err != nil {
 				o.Rollback()
-				beego.Error(err)
-				alertas = append(alertas, Alert{Type: AlertError, Body: "Error: ¡Ocurrió un error al insertar servicio necesidad!"})
 				return
 			}
 		}
-		o.Commit()
-		alertas = append(alertas, Alert{Type: AlertSucess, Body: m})
-
-		return
-	} else {
-		o.Rollback()
-		beego.Error(err)
-		alertas = append(alertas, Alert{Type: AlertError, Body: "Error: ¡Ocurrió un error al insertar la necesidad!"})
-		return
+		for _, vp := range m.ActividadEspecifica {
+			vp.Necesidad = &Necesidad{Id: int(id)}
+			//---
+			if id, err = o.Insert(vp); err != nil {
+				o.Rollback()
+				return
+			}
+		}
+		m.DetalleServicioNecesidad.Necesidad = &Necesidad{Id: int(id)}
+		if id, err = o.Insert(m.DetalleServicioNecesidad); err != nil {
+			o.Rollback()
+			return
+		}
 	}
-	return alertas, err
+	o.Commit()
+	return
+}
+
+//funcion para la transaccion de solicitudes
+func AddTrNecesidad(m *TrNecesidad) (alertas []Alert, err error) {
+	return TrNecesidadFunc(m)
+}
+
+//funcion para la transaccion de solicitudes
+func UpdateTrNecesidadById(m *TrNecesidad) (alertas []Alert, err error) {
+	return TrNecesidadFunc(m)
 }
